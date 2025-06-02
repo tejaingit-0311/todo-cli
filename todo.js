ae244d19eb0c4477ecf15b7e17089c8efac76f16
tejaingit-0311 <tejaworks03@gmail.com>
2025-06-02 16:19:54 +0530

    command:add has been updated

--- todo.js
+++ todo.js
@@ -1,13 +1,9 @@
 const { Command } = require('commander');
 const program = new Command(); 
 const fsPromises = require('fs').promises;
-let todos = [{
-    id: "",
-    status: false,
-    description: ""
-}]
+
 const filePath = './todos.json';
-let id = 0;
+
 //name a command
 //give description about what it does
 //set the version
@@ -22,31 +18,82 @@ program
     .command("add")
     .description("adds a specified description")
     .argument("<description>",  "Text describing the task, to be added.")
-    .action((newtask)=>{
+    .action(async (newTask)=>{
         //create a task object:
-        console.log(newtask);
-        const todo = {
-            id: ++id,
-            status: false,
-            description: newtask       
+        //console.log(newtask);
+        console.log(newTask);
+        try{
+            
+            let data = await fsPromises.readFile(filePath, "utf-8");
+            //data already present:
+            if(data){
+                //parse JSON into Object and extract array from it:
+                let todos = JSON.parse(data).todos;
+                //push the created object into the array
+                const todo = {
+                    id: parseInt(generateId()),
+                    status: false,
+                    description: newTask       
+                }
+                if(todos.some((t)=> t.id === todo.id)){
+                    throw "Id Already Present";    
+                }
+                todos.push(todo);
+                //parse back into JSON:
+                const todoJson = JSON.stringify({todos});
+                //write into the file:
+               await fsPromises.writeFile(filePath, todoJson);
+               console.log("Task Added successfully...");
+            }
+            //data is not present:
+            else{
+                //create a todObbject:
+                const todo = {
+                    id: parseInt(generateId()),
+                    status: false,
+                    description: newTask       
+                }
+                //place it in an array:
+                let todos = [todo];
+                //parse object into JSON:
+                const todosData = JSON.stringify({todos});
+                //write into the file:
+                await fsPromises.writeFile(filePath,todosData);
+                console.log("Task Added successfully...");
+            }
+
+        }catch(error){ 
+            console.error(error);
         }
-        
-        // add the task
-        todos.push(todo);
-        console.log("Task added:", newtask);
-        console.log(todos);
+
     })
 
 //view-command:
 program
     .command("list")
     .description("list all the tasks")
-    .action(async ()=>{
+    .option("-c, --completed", "gets only completed tasks", false)
+    
+    //two approaches using: 1. _,command 2. this with function 
+    .action(async function (_, command){
         try{
+            const options = command.opts();
+
             //get the data:
             let data = await fsPromises.readFile(filePath,'utf-8');
             //parse into object:
+           
                 let todos = JSON.parse(data).todos;
+               //to view only completed tasks:
+                if(options.completed){
+                    //get only completed tasks
+                    todos.forEach((todo)=>{
+                        if(todo.status === true){
+                            console.log(`Description: ${todo.description}`);
+                        }
+                    });
+                    
+                }
             //display the data
                todos.forEach((todo)=>{
                 const status = todo.status ? "Completed" : "Not Completed";
@@ -68,6 +115,7 @@ program
     .action(async (idArg, description)=>{
         //parse into number:
         let id = parseInt(idArg, 10);
+        console.log(id);
         try{
             //read file to get data:
             let data = await fsPromises.readFile(filePath, "utf-8");
@@ -99,6 +147,7 @@ program
     .action(async (idArg)=>{
         //covert idArg into number:
         const id = parseInt(idArg);
+       
         try{
             //read-file to get data:
             let data = await fsPromises.readFile(filePath,"utf-8");
@@ -123,4 +172,14 @@ program
             console.error(`Failed to update as: ${error}`);
         }
     })
+
+function generateId(){
+    const randomString = "1234567890";
+    let id = "";
+    for(let i = 0; i <= 1; i++){
+        id += randomString.charAt(Math.ceil(Math.random() * randomString.length));
+    }
+    return id;
+}
+
 program.parse();
